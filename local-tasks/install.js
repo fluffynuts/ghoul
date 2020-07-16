b045d02c897b70fba8c1f945858605917e38f82e
Davyd McColl <davydm@gmail.com>
2020-07-16 09:01:11 +0200

    :arrow_up: upgrade to zarro (install link task is broken tho because of node-win-shortcut and gyp

--- local-tasks/install.js
+++ local-tasks/install.js
@@ -1,10 +1,11 @@
 const
     gulp = requireModule("gulp-with-help"),
-    windowsShortcut = require("node-win-shortcut")
+    //windowsShortcut = require("node-win-shortcut")
     path = require("path"),
     installFolder = path.join(process.env.LOCALAPPDATA, "Ghoul");
 
 gulp.task("install", `Installs the binaries to ${installFolder}`, ["build"], () => {
+    throw new Error("FIXME: node-win-shortcut is broken (gyp install), so this task needs some <3");
     var binFolder = path.join("src", "Ghoul", "bin", "Debug");
     gulp.src([path.join(binFolder, "*"), `!${path.join(binFolder, "ghoul.ini")}`])
     .pipe(gulp.dest(installFolder));
